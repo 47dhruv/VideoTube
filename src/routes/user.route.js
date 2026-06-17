52d6d96396e413bee6c33375dacf7192d9040cce
47dhruv <dhruvpandey744@gmail.com>
2026-06-17 18:23:56 +0530

    Enhance user routes: add new endpoints for changing password, fetching current user, updating account details, and retrieving user watch history

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import {login, logOut,refreshAccessToken, register} from "../controllers/user.controller.js";
+import {changePassword, getCurrentUser, getUserChannelProfile, getWatchHistory, login, logOut,refreshAccessToken, register, updateAccountdetails, updateAvtar, updateCoverImage} from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import multer from "multer";
 
 const router = Router()
 
@@ -23,5 +24,12 @@ router.route("/register").post(
 
     //secured routes
     router.route("/logout").post(verifyJWT,logOut)
-    router.route("/refresh-Token").post(refreshAccessToken)
+    router.route("/refresh-Token").post(refreshAccessToken)  
+    router.route("/change-Password").post(verifyJWT,changePassword)
+    router.route("/currentUser").get(verifyJWT,getCurrentUser)
+    router.route("/updateAccountdetails").patch(verifyJWT,updateAccountdetails)
+    router.route("/update-Avatar").patch(verifyJWT,upload.single("avatar"),updateAvtar)
+    router.route("/update-CoverImage").patch(verifyJWT,upload.single("coverImage"),updateCoverImage)
+    router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+    router.route("/getUser-WatchHistory").post(verifyJWT,getWatchHistory)
 export default router
